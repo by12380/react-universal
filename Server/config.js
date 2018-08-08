d18e4e3ae857da9236d0b06f8b2f1b409eced78e
Brian Young <by12380@gmail.com>
2018-08-08 00:03:58 -0700

    Connect app to mongodb

--- Server/config.js
+++ Server/config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = process.env.DATABASE_URL;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/react-universal';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
 exports.PORT = process.env.PORT || 8080;
