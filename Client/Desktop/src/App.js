b03e97c325cc1190d7ef2fb1ff385d89789c0571
Brian Young <by12380@gmail.com>
2018-08-15 15:07:35 -0700

    Setup routing

--- Client/Desktop/src/App.js
+++ Client/Desktop/src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import Profile from './components/Profile';
+import LogIn from './components/LogIn';
+import Callback from './components/Callback';
+
 import logo from './logo.svg';
 import './App.css';
 
 const {app} = window.require('electron').remote;
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+          </div>
+          <p className="App-intro">
+            <b> Release 0.2.7 </b>
+            Version: {app.getVersion()}
+          </p>
+          <main>
+            <div>
+              <Route exact path="/" component={Profile} />
+              <Route exact path="/login" component={LogIn} />
+              <Route exact path="/callback" component={Callback} />
+            </div>
+          </main>
         </div>
-        <p className="App-intro">
-          <b> Release 0.2.7 </b>
-          Version: {app.getVersion()}
-        </p>
-      </div>
+      </Router>
     );
   }
 }
 
-export default App;
+export default connect()(App);
