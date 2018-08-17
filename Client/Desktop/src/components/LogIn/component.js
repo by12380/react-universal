d122630289c5b45d79cb715bb217f0349faaaff9
Brian Young <by12380@gmail.com>
2018-08-17 02:04:10 -0700

    Add login button

--- Client/Desktop/src/components/LogIn/component.js
+++ Client/Desktop/src/components/LogIn/component.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { auth } from '../../utils/auth0';
 
 const LogIn = (props) => {
+    const onClick = () => {
+        auth.authorize();
+    }
+
     return (
         <div>
             <p>LogIn page</p>
+            <button onClick={onClick}>
+                Log In
+            </button>
         </div>
     );
 }
