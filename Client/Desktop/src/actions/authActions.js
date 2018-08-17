de45ed796f51f03f82a4af3279fd2eb1863d4a31
Brian Young <by12380@gmail.com>
2018-08-17 13:56:47 -0700

    Add login by renew access token

--- Client/Desktop/src/actions/authActions.js
+++ Client/Desktop/src/actions/authActions.js
@@ -1,3 +1,5 @@
+import { auth } from '../utils/auth0';
+
 export const loginSuccess = (sessionItems) => {
     return {
         type: 'LOGIN_SUCCESS',
@@ -10,3 +12,40 @@ export const loginError = () => {
         type: 'LOGIN_ERROR',
     };
 };
+
+export const renewTokenPending = () => {
+    return {
+        type: 'RENEW_TOKEN_PENDING'
+    };
+};
+
+export const renewTokenSuccess = (sessionItems) => {
+    return {
+        type: 'RENEW_TOKEN_SUCCESS',
+        sessionItems
+    };
+};
+
+export const renewTokenError = () => {
+    return {
+        type: 'RENEW_TOKEN_ERROR'
+    };
+};
+
+export const renewToken = () => (dispatch) => {
+    dispatch(renewTokenPending());
+    auth.checkSession({}, (err, authResult) => {
+        if (err) {
+            dispatch(renewTokenError());
+        } else {
+            authResult.expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+            const sessionItems = {
+                accessToken: authResult.accessToken,
+                idToken: authResult.idToken,
+                expiresAt: authResult.expiresAt
+            }
+            dispatch(renewTokenSuccess(sessionItems));
+        }
+    })
+}
+
