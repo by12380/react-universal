aaacf0e49944ddcbddcd0e4e8147ecd15529f226
Brian Young <by12380@gmail.com>
2018-08-17 16:10:21 -0700

    Add fetch user

--- Client/Desktop/src/components/Profile/index.js
+++ Client/Desktop/src/components/Profile/index.js
@@ -1,12 +1,26 @@
 import Profile from "./component";
 import { connect } from "react-redux";
+import { bindActionCreators } from 'redux';
+import { fetchUser } from '../../actions/userActions';
 
 const mapStateToProps = (state) => {
 
   return {
-
+    pending: state.userReducer.pending,
+    success: state.userReducer.success,
+    error: state.userReducer.error,
+    profile: state.userReducer.profile,
+    token: state.authReducer.sessionItems.accessToken
   };
 
 };
 
-export default connect(mapStateToProps)(Profile);
+const mapDispatchToProps = (dispatch) => {
+
+  return bindActionCreators({
+    fetchUser
+  }, dispatch);
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
