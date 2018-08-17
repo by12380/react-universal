ba2ff00a66aa3488d6a55bd6ab5dfe2dd4851059
Brian Young <by12380@gmail.com>
2018-08-17 00:23:47 -0700

    Add function to retrieve token from auth callback

--- Client/Desktop/src/utils/auth0.js
+++ Client/Desktop/src/utils/auth0.js
@@ -1,13 +1,26 @@
 import auth0 from 'auth0-js';
 import { AUTH0_DOMAIN, AUTH0_CLIENT_ID, AUTH0_API_AUDIENCE, AUTH0_REDIRECT_URL } from '../config';
 
-const auth = new auth0.WebAuth({
+export const auth = new auth0.WebAuth({
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
     redirectUri: AUTH0_REDIRECT_URL,
     audience: AUTH0_API_AUDIENCE,
     responseType: 'token id_token',
-    scope: 'openid profile'
+    scope: 'openid'
 });
 
-module.exports = { auth }
+export const getTokensFromAuthCallbackAsync = () => {
+    return new Promise((resolve, reject) => {
+        auth.parseHash((err, result) => {
+            if (result && result.accessToken && result.idToken) {
+                //Add a new field 'expiresAt' that gives the datetime the token expires.
+                result.expiresAt = JSON.stringify((result.expiresIn * 1000) + new Date().getTime());
+                resolve(result);
+            } else if (err) {
+                console.log(err);
+                reject(err);
+            }
+        });
+    })
+};
