66db4ebe709975ea2d2d5a1e54c91c964d3ed995
Brian Young <by12380@gmail.com>
2018-08-17 01:43:02 -0700

    Handle auth callback

--- Client/Desktop/src/components/Callback/index.js
+++ Client/Desktop/src/components/Callback/index.js
@@ -1,12 +1,24 @@
 import Callback from "./component";
+import { bindActionCreators } from 'redux';
 import { connect } from "react-redux";
+import { loginSuccess, loginError } from '../../actions/authActions';
 
 const mapStateToProps = (state) => {
 
   return {
-
+    success: state.authReducer.success,
+    error: state.authReducer.error
   };
 
 };
 
-export default connect(mapStateToProps)(Callback);
+const mapDispatchToProps = (dispatch) => {
+
+  return bindActionCreators({
+    loginSuccess,
+    loginError
+  }, dispatch);
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Callback);
