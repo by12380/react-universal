ca241c70f65ce570ad2ae8c8ea3b55e0d90d7a72
$quirrel <Niko.Tzikas@gmail.com>
2018-08-18 02:26:50 -0400

    Revert "Sign in page"

--- Client/Mobile/navigation/AppNavigator.js
+++ Client/Mobile/navigation/AppNavigator.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
-import LogInScreen from '../screens/LogInScreen';
-import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
-const AuthStack = createStackNavigator({ LogIn: LogInScreen });
-
-export default createSwitchNavigator(
-  {
+export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  AuthLoading: AuthLoadingScreen,
   Main: MainTabNavigator,
-  Auth: AuthStack
-  },
-  {
-    initialRouteName: 'AuthLoading',
-  }
-);
+});
