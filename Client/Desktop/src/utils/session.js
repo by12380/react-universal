e25865e7848ceff5e3d53f435c5dca2a5d79ebb5
Brian Young <by12380@gmail.com>
2018-08-19 22:53:56 -0700

    Add get/set session utils

---
+++ Client/Desktop/src/utils/session.js
@@ -0,0 +1,18 @@
+const storage = window.require('electron-json-storage');
+
+export const storeSession = (sessionItems) => {
+    storage.set('session', sessionItems, function(error) {
+        console.error(error);
+    });
+}
+
+export const getSessionAsync = () => {
+    return new Promise((resolve, reject) => {
+        storage.get('session', function(error, data) {
+            if (error) {
+                reject(error);
+            }
+            resolve(data);
+        });
+    })
+}
