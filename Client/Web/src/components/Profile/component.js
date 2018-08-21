86ef57e6f559e30d3c9f8f255d8931d3ed66f727
Brian Young <by12380@gmail.com>
2018-08-20 20:31:50 -0700

    Transfer and modify code from electron project

---
+++ Client/Web/src/components/Profile/component.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { logOut } from '../../utils/auth0';
+import './Profile.css';
+
+class Profile extends Component {
+
+    componentDidMount() {
+        this.props.fetchUser(this.props.token)
+    }
+
+    onLogOut = () => {
+        this.props.removeSession();
+        logOut();
+    }
+
+    render() {
+        return (
+            <div>
+                { this.props.profile.picture
+                ? <img className='profile-img' src={this.props.profile.picture} alt='User Profile'/>
+                : null }
+                
+                { this.props.profile.name
+                ? <p>Welcome, {this.props.profile.name}</p>
+                : null }
+
+                <button className='logout-btn' onClick={this.onLogOut}>Logout</button>
+            </div>
+        );
+    }
+}
+
+export default Profile;
