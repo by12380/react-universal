c566ede0e3f79653ebdb99261845255cf209ab05
Brian Young <by12380@gmail.com>
2018-08-28 16:18:28 -0700

    Add store user to database functionality

--- Client/React/src/actions/userActions.js
+++ Client/React/src/actions/userActions.js
@@ -1,4 +1,5 @@
 import { auth } from '../utils/auth0';
+import { APP_SERVER_URL } from '../config';
 
 const fetchUserPending = () => {
     return {
@@ -19,6 +20,24 @@ const fetchUserError = () => {
     }
 }
 
+const storeUserPending = () => {
+    return {
+        type: 'STORE_USER_PENDING'
+    }
+}
+
+const storeUserSuccess = () => {
+    return {
+        type: 'STORE_USER_SUCCESS'
+    }
+}
+
+const storeUserError = () => {
+    return {
+        type: 'STORE_USER_ERROR'
+    }
+}
+
 export const fetchUser = (accessToken) => (dispatch) => {
     dispatch(fetchUserPending());
     auth.client.userInfo(accessToken, (err, result) => {
@@ -31,6 +50,35 @@ export const fetchUser = (accessToken) => (dispatch) => {
                 picture: result.picture
             }
             dispatch(fetchUserSuccess(profile));
+            dispatch(storeUser(accessToken, profile));
         }
     })
+}
+
+export const storeUser = (accessToken, user) => (dispatch) => {
+
+    dispatch(storeUserPending());
+
+    fetch(`${APP_SERVER_URL}/users/update`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+            email: user.email,
+        }),
+    })
+    .then(res => {
+        if (res.status !== 200)
+            throw `failed to store user with status ${res.status}`;
+        return res.json();
+    })
+    .then(result => {
+        dispatch(storeUserSuccess());
+    })
+    .catch(error => {
+        dispatch(storeUserError());
+    })
+
 }
