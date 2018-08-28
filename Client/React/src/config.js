1f428597dba96ec246f76e16889250c6d59763b2
Brian Young <by12380@gmail.com>
2018-08-28 16:18:28 -0700

    Add store user to database functionality

--- Client/React/src/config.js
+++ Client/React/src/config.js
@@ -2,4 +2,7 @@ export const AUTH0_DOMAIN = 'react-universal.auth0.com';
 export const AUTH0_CLIENT_ID = '3821inroTsXub5iKOew7JHTT-OaJl3jp';
 export const AUTH0_API_AUDIENCE = 'https://api.react-universal.com';
 export const AUTH0_REDIRECT_URL = 'http://localhost:3000/callback';
-export const HOME_URL = 'http://localhost:3000/'
+export const HOME_URL = 'http://localhost:3000/';
+
+//App server
+export const APP_SERVER_URL = process.env.APP_SERVER_URL || 'http://localhost:8080';
