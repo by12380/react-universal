20d6b01e798fe82bd3a5efa5becc3a6fdb00c376
Brian Young <by12380@gmail.com>
2018-09-06 12:36:32 -0700

    Update app config var

--- Client/Electron/src/config.js
+++ Client/Electron/src/config.js
@@ -1,7 +1,7 @@
-export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
-export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
-export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET;
-export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE;
+export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN || 'react-universal.auth0.com';
+export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID || '3821inroTsXub5iKOew7JHTT-OaJl3jp';
+export const AUTH0_CLIENT_SECRET = process.env.REACT_APP_AUTH0_CLIENT_SECRET || '3usxeq-Ae3jur3LYVlhJ0Ldjyi01sCoHsbTdRpbFLFwzeRMZjQHCp8La1ixDx9jW';
+export const AUTH0_API_AUDIENCE = process.env.REACT_APP_AUTH0_API_AUDIENCE || 'https://api.react-universal.com';
 export const AUTH0_LOGIN_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGIN_REDIRECT_URL || 'http://localhost:3000/callback';
 export const AUTH0_LOGOUT_REDIRECT_URL = process.env.REACT_APP_AUTH0_LOGOUT_REDIRECT_URL || 'http://localhost:3000/';
 
