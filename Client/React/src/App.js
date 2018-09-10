a42b800ec3d5eedf279af8542d819133bf5f406c
Brian Young <by12380@gmail.com>
2018-09-10 14:48:17 -0700

    Revert "Setup socket io on React web"
    This reverts commit 35dfc8ced2dd65bd66a6f35c87dee42bbae9d9ac.

--- Client/React/src/App.js
+++ Client/React/src/App.js
@@ -2,41 +2,24 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import io from 'socket.io-client';
 
 import Profile from './components/Profile';
 import LogIn from './components/LogIn';
 import Callback from './components/Callback';
 import { loadSession } from './actions/authActions';
-import { APP_SERVER_URL } from './config';
-import { configSocketIO } from './socket-io';
 
 import logo from './logo.svg';
 import './App.css';
-
-const socket = io(APP_SERVER_URL);
 
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      scoketConnected: false
-    }
-
     props.loadSession();
   }
 
-  componentDidUpdate() {
-    if (this.props.isAuthenticated && !this.state.scoketConnected) {
-      configSocketIO(socket, {user_id: this.props.user_id});
-      this.setState({scoketConnected: true});
-    }
-  }
-
   render() {
-
     return (
       <Router>
         <div className="App">
@@ -61,7 +44,6 @@ const mapStateToProps = (state) => {
     isAuthenticated:
       new Date().getTime() <
       (state.authReducer.sessionItems ? state.authReducer.sessionItems.expiresAt : null),
-    user_id: state.userReducer.profile.sub
   }
 }
 
--- Client/React/src/index.js
+++ Client/React/src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import './index.css';
 
 //create the redux store
-export const store = createStore(
+const store = createStore(
     reducers,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     applyMiddleware(thunk)
