c477eea431482591b6589188ad89b763ae4fb501
Brian Young <by12380@gmail.com>
2018-09-13 23:48:34 -0700

    Add socket io to electron

--- Client/Electron/src/App.js
+++ Client/Electron/src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import io from 'socket.io-client';
 
 import Profile from './components/Profile';
 import LogIn from './components/LogIn';
 import { loadSession } from './actions/authActions';
+import { joinRoomSuccess } from './actions/socketActions';
+import { APP_SERVER_URL } from './config';
+import { initSubscriber } from './subscriber-client';
 
 import logo from './logo.svg';
 import './App.css';
+
+const socket = io(APP_SERVER_URL);
 
 class App extends Component {
 
@@ -16,6 +22,14 @@ class App extends Component {
     super(props);
 
     props.loadSession();
+  }
+
+  componentDidMount() {
+    initSubscriber(socket);
+  }
+
+  componentDidUpdate() {
+    this.onSocketConnect();
   }
 
   render() {
@@ -36,6 +50,16 @@ class App extends Component {
       </Router>
     );
   }
+
+  onSocketConnect = () => {
+    if (
+      this.props.socketConnected
+      && this.props.user_id
+      && !this.props.roomJoined ) {
+        socket.emit('room', this.props.user_id);
+        this.props.joinRoomSuccess();
+    }
+  }
 }
 
 const mapStateToProps = (state) => {
@@ -43,13 +67,17 @@ const mapStateToProps = (state) => {
     isAuthenticated:
       new Date().getTime() <
       (state.authReducer.sessionItems ? state.authReducer.sessionItems.expiresAt : null),
+    user_id: state.userReducer.user.sub,
+    socketConnected: state.socketReducer.connectSuccess,
+    roomJoined: state.socketReducer.joinRoomSuccess
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
 
   return bindActionCreators({
-    loadSession
+    loadSession,
+    joinRoomSuccess
   }, dispatch);
 
 }
--- Client/Electron/src/reducers/index.js
+++ Client/Electron/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from './authReducer';
 import userReducer from './userReducer';
+import socketReducer from './socketReducer';
 
 export default combineReducers({
     authReducer,
-    userReducer
+    userReducer,
+    socketReducer
 });
